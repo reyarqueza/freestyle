0d965b5162e8d172446d1baaf538480eabe32da3
Rey Arqueza <reyarqueza@gmail.com>
2022-07-03 11:10:06 -0700

    react controlled component

--- react/hooks/src/index.js
+++ react/hooks/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ToggleStatus from "./jsx/ToggleStatus.jsx";
+import ControlledTextInput from "./jsx/ControlledTextInput.jsx";
 
 console.log("React.version", React.version);
 ReactDOM.render(
@@ -8,6 +9,7 @@ ReactDOM.render(
     <h1>Hooks Examples</h1>
     <hr />
     <ToggleStatus />
+    <ControlledTextInput />
   </div>,
   document.getElementById("root")
 );
