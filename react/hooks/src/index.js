611201835e834c1592afad8bac3c009589456b40
Rey Arqueza <reyarqueza@gmail.com>
2022-07-11 16:08:35 -0700

    practice

--- react/hooks/src/index.js
+++ react/hooks/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ToggleStatus from "./jsx/ToggleStatus.jsx";
+import Increment from './jsx/Increment.jsx';
+import ToDo from "./jsx/Todo.jsx";
 import ControlledTextInput from "./jsx/ControlledTextInput.jsx";
 
 console.log("React.version", React.version);
@@ -8,7 +10,9 @@ ReactDOM.render(
   <div>
     <h1>Hooks Examples</h1>
     <hr />
+    <Increment />
     <ToggleStatus />
+    <ToDo />
     <ControlledTextInput />
   </div>,
   document.getElementById("root")
