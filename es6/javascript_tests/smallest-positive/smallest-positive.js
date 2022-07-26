a0e9c28ecf5c654c9719d66994d4fd7d9cedeb44
Rey Arqueza <reyarqueza@gmail.com>
2022-07-26 12:26:31 -0700

    the first of a series of practice tests

--- es6/javascript_tests/smallest-positive/smallest-positive.js
+++ es6/javascript_tests/smallest-positive/smallest-positive.js
@@ -1,25 +1,10 @@
-function solution(A) {
-  let smallestPostive = 1;
-
-  const simplifiedArray = [...new Set(A)]
-    .filter(item => item > 0)
+export default function smallestPostive(numbers) {
+  const smallestPostive = 1;
+  const filteredAndSorted = [...new Set(numbers)]
+    .filter((item) => item > 0)
     .sort();
 
-  simplifiedArray.forEach((item, index) => {
-    if (item === index + 1) {
-      smallestPostive = index + 2;
-    }
-  });
-
-  return smallestPostive;
-
+    return filteredAndSorted && filteredAndSorted.length
+      ? filteredAndSorted.reduce((prev, curr) => prev < curr ? prev : curr)
+      : smallestPostive
 }
-
-const test1 = [1, 3, 6, 4, 1, 2];
-console.log(solution(test1));
-
-const test2 = [1,2,3];
-console.log(solution(test2));
-
-const test3 = [-1, -3];
-console.log(solution(test3));
