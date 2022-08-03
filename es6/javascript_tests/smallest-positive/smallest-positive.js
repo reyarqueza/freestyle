ea0a685c805d5ec3526e884caa485c6329db3a22
Rey Arqueza <reyarqueza@gmail.com>
2022-08-03 12:13:48 -0700

    smallest positive with tests

--- es6/javascript_tests/smallest-positive/smallest-positive.js
+++ es6/javascript_tests/smallest-positive/smallest-positive.js
@@ -1,10 +1,19 @@
-export default function smallestPostive(numbers) {
+export function smallestPositive1(numbers) {
   const filteredNums = numbers.filter((number) => number > 0);
 
   const smallestPos =
     filteredNums &&
     filteredNums.length > 0 &&
     filteredNums.reduce((prev, curr) => (prev < curr ? prev : curr));
+
+  return smallestPos ? smallestPos : 1;
+}
+
+export function smallestPositive2(numbers) {
+  const filteredNums = numbers.filter((number) => number > 0).sort();
+  console.log("filteredNums", filteredNums);
+  const smallestPos =
+    filteredNums && filteredNums.length > 0 && filteredNums[0];
 
   return smallestPos ? smallestPos : 1;
 }
